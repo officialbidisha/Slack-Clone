6eabfc43d76807208ca455c044b9f829dd0d2aac
Bidisha Das <officialbidisha1@gmail.com>
2022-01-10 09:33:59 +0530

    Added channels to firebase cloudstore

--- src/components/SideBarOption.js
+++ src/components/SideBarOption.js
@@ -1,18 +1,45 @@
-import React from 'react'
-import classes from './SideBarOption.module.css';
-function SideBarOption({Icon , title}) {
-    return (
-        <div className={classes['sidebar-option-container']}>
-            {Icon && <Icon fontSize="small" style={{padding: 10}}/>}
-            {Icon ? (
-                <h3>{title}</h3>
-            ): (
-                <h3 className={classes['sidebar-option-channel']}>
-                    <span>#</span> {title}
-                </h3>
-            )}
-        </div>
-    )
+import { doc, addDoc, setDoc } from "firebase/firestore/lite";
+import { getFirestore, collection } from "firebase/firestore";
+import React from "react";
+import { db, firebaseApp } from "../../src/firebase";
+import classes from "./SideBarOption.module.css";
+import { useCollection } from "react-firebase-hooks/firestore";
+function SideBarOption({ Icon, title, addChannelOption }) {
+  const [channels, loading, error] = useCollection(
+    collection(getFirestore(firebaseApp), "rooms"),
+    {
+      snapshotListenOptions: { includeMetadataChanges: true },
+    }
+  );
+
+  console.log('Channels', channels);
+
+  const addChannel = async () => {
+    const channelName = prompt("Please enter the channel name");
+    if (channelName) {
+      const docRef = await addDoc(collection(db, "rooms"), {
+        name: channelName,
+      });
+    }
+  };
+
+  const selectChannel = () => {};
+
+  return (
+    <div
+      className={classes["sidebar-option-container"]}
+      onClick={addChannelOption ? addChannel : selectChannel}
+    >
+      {Icon && <Icon fontSize="small" style={{ padding: 10 }} />}
+      {Icon ? (
+        <h3>{title}</h3>
+      ) : (
+        <h3 className={classes["sidebar-option-channel"]}>
+          <span>#</span> {title}
+        </h3>
+      )}
+    </div>
+  );
 }
 
-export default SideBarOption
+export default SideBarOption;
--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -14,4 +16,9 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const firebaseApp = initializeApp(firebaseConfig);
+const db = getFirestore(firebaseApp);
+const auth = getAuth(firebaseApp);
+const provider = new GoogleAuthProvider();
+
+export {db, auth, provider, firebaseApp};
